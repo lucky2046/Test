86aeb104c151482dcbca24e68a1f3fb0c8414e7e
lujun-wuhan <cuglujun@163.com>
2016-12-24 00:29:59 +0800

    Update fix.js

--- fix.js
+++ fix.js
@@ -1,5 +1,27 @@
-defineClass('LoginViewController', {
-  handleBtn: function(sender) {
-    self.alertViewWithMessage("扫码登录");
-  }
-})
+require('Config,LBXScanWrapper,SubLBXScanViewController');
+scan: function(sender) {
+    var url = Config.sharedInstance().value();
+    var pad_mac = Config.sharedInstance().mac();
+    if (url.length() > 0) {
+        self.qrcode({
+            "value": url,
+            "pad_mac": pad_mac
+        });
+        return;
+    }
+
+    if (!LBXScanWrapper.isGetCameraPermission()) {
+        self.alertViewWithTitle_message_buttonTitles_result("提示", "请在设备的设置-隐私-相机中允许访问相机。", null, null);
+        return;
+    }
+
+    var vc = SubLBXScanViewController.new();
+    vc.setTitle("扫码登录");
+    self.navigationController().pushViewController_animated(vc, YES);
+    vc.setQRUrlBlock(block('NSString*', function(url) {
+            self.alertViewWithMessage(url);
+            self.navigationController().popViewControllerAnimated(NO);
+            self.qrcodeLogin(url);
+        }
+    }));
+}
